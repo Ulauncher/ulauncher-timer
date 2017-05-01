64e106c57ad90f9f02e9941dfa9780846b7457b9
Aleksandr Gornostal <sanya.gornostal@gmail.com>
2017-05-01 10:30:39 +0300

    don't use dateutil

--- tests/timer/test_query_parser.py
+++ tests/timer/test_query_parser.py
@@ -3,15 +3,15 @@ from timer.query_parser import parse_query, ParseQueryError
 
 
 def test_parse_query__5m_hello_world__correct_result_returned():
-    assert parse_query('5m hello world') == (5 * 60, '5 minutes', 'hello world')
+    assert parse_query('5m hello world') == (5 * 60, '5m', 'hello world')
 
 
 def test_parse_query__5h_hello_world__correct_result_returned():
-    assert parse_query('5h hello world') == (5 * 60 * 60, '5 hours', 'hello world')
+    assert parse_query('5h hello world') == (5 * 60 * 60, '5h', 'hello world')
 
 
 def test_parse_query__5__is_correct_query():
-    assert parse_query('5') == (5 * 60, '5 minutes', 'Time is up!')
+    assert parse_query('5') == (5 * 60, '5', 'Time is up!')
 
 
 def test_parse_query__incorrect_query__ParseQueryError():
--- timer/query_parser.py
+++ timer/query_parser.py
@@ -1,5 +1,4 @@
 import re
-from dateutil.relativedelta import relativedelta
 
 
 TIME_MULT = {
@@ -7,17 +6,16 @@ TIME_MULT = {
     'm': 60,
     's': 1
 }
-ATTRS = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
 
 
 def parse_query(query, default_text='Time is up!'):
     """
     >>> '5m hello world'
-    <<< (300, '5 minutes', 'hello world')
+    <<< (300, '5m', 'hello world')
     >>> '3h Go'
-    <<< (3*60*60, '3 hours', 'Go')
+    <<< (3*60*60, '3h', 'Go')
     >>> '5'
-    <<< (300, '5 minutes', 'Time is up!')
+    <<< (300, '5', 'Time is up!')
     """
     try:
         time_arg = query.split(' ')[0]
@@ -28,19 +26,9 @@ def parse_query(query, default_text='Time is up!'):
         message = ' '.join(query.split(' ')[1:]).strip()
         message = message or default_text
 
-        return (time_sec, ' '.join(human_readable(time_sec)), message)
+        return (time_sec, time_arg, message)
     except Exception as e:
         raise ParseQueryError(e.message)
-
-
-def human_readable(seconds):
-    """
-    >>> 125
-    <<< ['2 hours', '5 minutes']
-    """
-    delta = relativedelta(seconds=seconds)
-    return ['%d %s' % (getattr(delta, attr), getattr(delta, attr) > 1 and attr or attr[:-1])
-            for attr in ATTRS if getattr(delta, attr)]
 
 
 class ParseQueryError(Exception):
