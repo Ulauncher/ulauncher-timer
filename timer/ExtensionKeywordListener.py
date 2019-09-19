8da1bd719f4d43e2f91eefc09efd7b1b0cb85d80
Aleksandr Gornostal <al.gornostal@gmail.com>
2019-09-19 14:54:46 +0300

    Revert "Show "time left" if timer is running (#5)" (#6)
    This reverts commit 719fa50314283b70f42f7e30086af584e943314a.

--- timer/ExtensionKeywordListener.py
+++ timer/ExtensionKeywordListener.py
@@ -4,14 +4,12 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from .query_parser import parse_query, ParseQueryError
-from .timediff_formatter import format_timediff
 
 
 class ExtensionKeywordListener(EventListener):
 
-    def __init__(self, icon_file, get_timer):
+    def __init__(self, icon_file):
         self.icon_file = icon_file
-        self.get_timer = get_timer
 
     def get_action_to_render(self, name, description, on_enter=None):
         item = ExtensionResultItem(name=name,
@@ -32,10 +30,6 @@ class ExtensionKeywordListener(EventListener):
             except ParseQueryError:
                 return self.get_action_to_render(name="Incorrect request",
                                                  description="Example: ti 10m Eggs are ready!")
-        elif time_left is not None:
-            timediff_str = format_timediff(self.get_timer())
-            return self.get_action_to_render(name="A Timer is running",
-                                             description= "Time left: %s" % timediff_str)
         else:
             return self.get_action_to_render(name="Type in your query",
                                              description="Example: ti 10m Eggs are ready!")
