27fd8b0a4a98befd107a514ab45364d325a040d6
Carsten Csiky <carsten.csiky@gmail.com>
2019-09-25 08:42:51 +0200

    Show time left (#7)
    * show time left in timer
    
    * add timediff formatter
    
    * fixes bug: `time_left` not defined

--- timer/ExtensionKeywordListener.py
+++ timer/ExtensionKeywordListener.py
@@ -4,12 +4,14 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from .query_parser import parse_query, ParseQueryError
+from .timediff_formatter import format_timediff
 
 
 class ExtensionKeywordListener(EventListener):
 
-    def __init__(self, icon_file):
+    def __init__(self, icon_file, get_timer):
         self.icon_file = icon_file
+        self.get_timer = get_timer
 
     def get_action_to_render(self, name, description, on_enter=None):
         item = ExtensionResultItem(name=name,
@@ -21,6 +23,7 @@ class ExtensionKeywordListener(EventListener):
 
     def on_event(self, event, extension):
         query = event.get_argument()
+        time_left = self.get_timer()
         if query:
             try:
                 time_sec, delta, message = parse_query(query)
@@ -30,6 +33,10 @@ class ExtensionKeywordListener(EventListener):
             except ParseQueryError:
                 return self.get_action_to_render(name="Incorrect request",
                                                  description="Example: ti 10m Eggs are ready!")
+        elif time_left is not None:
+            timediff_str = format_timediff(time_left)
+            return self.get_action_to_render(name="A Timer is running",
+                                             description= "Time left: %s" % timediff_str)
         else:
             return self.get_action_to_render(name="Type in your query",
                                              description="Example: ti 10m Eggs are ready!")
