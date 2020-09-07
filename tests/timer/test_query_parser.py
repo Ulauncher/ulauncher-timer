3d9c2ceb168236023d0171695bcbea1a3f906855
Daniel Miller <millerdev@gmail.com>
2020-09-07 12:02:36 -0400

    Add time of day parser

--- tests/timer/test_query_parser.py
+++ tests/timer/test_query_parser.py
@@ -1,5 +1,6 @@
 import pytest
 from timer.query_parser import parse_query, ParseQueryError
+from util import patch_now
 
 
 def test_parse_query__5m_hello_world__correct_result_returned():
@@ -38,3 +39,36 @@ def test_parse_query__incorrect_query__ParseQueryError():
 def test_parse_query__incorrect_query_with_no_units__ParseQueryError():
     with pytest.raises(ParseQueryError):
         assert parse_query('1h3')
+
+
+def secs(hours, minutes=0):
+    return hours * 60 * 60 + minutes * 60
+
+
+@pytest.mark.parametrize("query,result", [
+    ("5p", secs(5)),
+    ("5pm", secs(5)),
+    ("530pm", secs(5, 30)),
+    ("5:30p", secs(5, 30)),
+    ("310p", secs(3, 10)),
+    ("1010p", secs(10, 10)),
+    ("10a", secs(22,)),
+    ("1156a", secs(23, 56)),
+    ("1203a", secs(0, 3)),
+])
+@patch_now
+def test_parse_query__absolute_time__correct_result_returned(query, result):
+    assert parse_query(f'{query} hello') == (result, query, 'hello')
+
+
+@pytest.mark.parametrize("query,message", [
+    ("5px", "Bad time format: 5px"),
+    ("13p", "Bad time value: 13p"),
+    ("25am", "Bad time value: 25am"),
+    ("560p", "Bad time value: 560p")
+])
+@patch_now
+def test_parse_query__absolute_time__error(query, message):
+    with pytest.raises(ParseQueryError, match=message):
+        value = parse_query(f'{query} hello')
+        assert 0, f"unexpected value: {value!r}"
