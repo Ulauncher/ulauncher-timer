c9b89a422d8922ed6e408975ad9427e9be970546
Daniel Miller <millerdev@gmail.com>
2020-09-27 21:33:20 -0400

    Uninitialize Notify on exit

--- timer/TimerExtension.py
+++ timer/TimerExtension.py
@@ -1,12 +1,23 @@
+import logging
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Notify', '0.7')
 
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from gi.repository import Notify
+
+from ulauncher.api.shared.event import (
+    ItemEnterEvent,
+    KeywordQueryEvent,
+    SystemExitEvent,
+)
+from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.client.Extension import Extension
 from .ExtensionKeywordListener import ExtensionKeywordListener
 from .ItemEnterEventListener import ItemEnterEventListener
 from .Timer import Timer
+
+log = logging.getLogger(__name__)
 
 
 class TimerExtension(Extension):
@@ -16,6 +27,7 @@ class TimerExtension(Extension):
         self.timers = set()
         self.subscribe(KeywordQueryEvent, ExtensionKeywordListener(self.get_timers))
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+        self.subscribe(SystemExitEvent, SystemExitEventListener())
 
     def set_timer(self, delay, text):
         timer = Timer(delay, text, self.on_timer_end)
@@ -31,3 +43,10 @@ class TimerExtension(Extension):
 
     def get_timers(self):
         return sorted(self.timers, key=lambda t: t.end_time)
+
+
+class SystemExitEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        log.debug("Uninitialize notifications")
+        Notify.uninit()
