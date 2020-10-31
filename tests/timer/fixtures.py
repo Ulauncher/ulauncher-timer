273e939ba75bf588c51084bac80b325361d03686
Daniel Miller <millerdev@gmail.com>
2020-10-31 15:40:34 -0400

    Support @using with @pytest.mark.parametrize

--- tests/timer/fixtures.py
+++ tests/timer/fixtures.py
@@ -3,6 +3,7 @@
 See also https://github.com/pytest-dev/pytest/issues/3834
 """
 from contextlib import contextmanager, ExitStack
+from functools import wraps
 
 __all__ = ["fixture", "using"]
 
@@ -15,13 +16,26 @@ def using(*fixtures):
         raise TypeError("at least one fixture is required")
 
     def apply_fixtures(test):
-        def test_func():
+        @wraps(test)
+        def test_func(*args, **kw):
             with ExitStack() as stack:
-                args = [stack.enter_context(f()) for f in fixtures]
-                test(*args)
-        test_func.__doc__ = test.__doc__
-        test_func.__module__ = test.__module__
-        test_func.__name__ = test.__name__
-        test_func.__qualname__ = test.__qualname__
+                fixture_args = [stack.enter_context(f()) for f in fixtures]
+                test(*fixture_args, *args, **kw)
+        test.nonmagic_fixtures_count = len(fixtures)
+        test_func.nonmagic_fixtures_count = len(fixtures)
         return test_func
     return apply_fixtures
+
+
+def _patch_pytest_num_mock_patch_args():
+    # HACK support @pytest.mark.parametrize(...) on @using(...)-wrapped tests
+    def num_mock_patch_args(function):
+        num = getattr(function, "nonmagic_fixtures_count", 0)
+        return num + real_num_mock_patch_args(function)
+
+    import _pytest.compat as compat
+    real_num_mock_patch_args = compat.num_mock_patch_args
+    compat.num_mock_patch_args = num_mock_patch_args
+
+
+_patch_pytest_num_mock_patch_args()
