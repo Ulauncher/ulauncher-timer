2d3fe82fe1e38670e6eb8887cd2a4b4d1c2baa79
Bruno Duyé <brunetton@gmail.com>
2023-03-25 09:07:41 +0100

    Issue 21 fix repeating notifications (#22)
    * Fix #21: do not stop timer if notification is closed by timeout (not user)
    
    * Code refactoring: move Timer.on_close() outside Timer.start() and rename it
    
    ---------
    
    Co-authored-by: Bruno Duyé <brunetton@brunetton.org>

--- timer/TimerExtension.py
+++ timer/TimerExtension.py
@@ -32,15 +32,15 @@ class TimerExtension(Extension):
     def set_timer(self, delay, text):
         log.debug("add timer %s %s", delay, text)
         persistent = self.preferences["persistent"]
-        timer = Timer(delay, text, self.on_timer_end, persistent)
-        timer.start(self.loop)
+        timer = Timer(delay, text, self.on_timer_end, self.loop, persistent)
+        timer.start()
         self.timers.add(timer)
 
     def stop_timer(self, timer_id):
         timer = self.get_timer(timer_id)
         if timer is not None:
             log.debug("stop timer %s", timer.name)
-            timer.stop(self.loop, notify=True)
+            timer.stop(notify=True)
             self.timers.remove(timer)
 
     def on_timer_end(self, timer):
